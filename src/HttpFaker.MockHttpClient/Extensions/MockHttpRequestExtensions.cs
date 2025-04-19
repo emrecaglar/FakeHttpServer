da79153281b992a29e1b8487fd663b14df582981
Emre ÇAĞLAR <emre_caglar@msn.com>
2025-04-19 19:14:09 +0300

    add MockHttpRequest extension methods

---
+++ src/HttpFaker.MockHttpClient/Extensions/MockHttpRequestExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Primitives;
+using System;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace HttpFaker.MockHttpClient.Extensions
+{
+    public static class MockHttpRequestExtensions
+    {
+        public static bool TryGetQueryStringValues(this MockHttpRequest req, string key, out StringValues values)
+        {
+            var qs = req.QueryString.FirstOrDefault(x => x.Key.Equals(key, StringComparison.OrdinalIgnoreCase));
+
+            if (qs.Key == null)
+            {
+                values = StringValues.Empty;
+                return false;
+            }
+
+            values = qs.Value;
+            return true;
+        }
+
+        public static string GetValueAsString(this HttpRequestHeaders header, string name)
+        {
+            return header.GetValues(name).ToStringValues().ToString();
+        }
+    }
+}
---
+++ src/HttpFaker.MockHttpClient/Extensions/SringEnumerableExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Primitives;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HttpFaker.MockHttpClient.Extensions
+{
+    public static class SringEnumerableExtensions
+    {
+        public static StringValues ToStringValues(this IEnumerable<string> values)
+        {
+            return new StringValues(values.ToArray());
+        }
+    }
+}
